2ce9f27276151f75c7fad0f6cf5eb1949f64175b
Nicolas Quispe <nicolasq1477@gmail.com>
2022-09-22 19:56:53 -0300

    borrado de funcionalidad

--- js/api-farmacias.js
+++ js/api-farmacias.js
@@ -44,19 +44,3 @@ req.addEventListener('load', e => {
 });
 req.send();
 
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
