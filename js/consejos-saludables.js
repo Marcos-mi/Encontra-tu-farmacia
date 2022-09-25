e571b6e8cffae665788d63bdc3bb93ef0bc6a93d
Juan Yovera <juanyovera15@gmail.com>
2022-09-25 20:37:59 -0300

    hago dinciones de consejos saludables

---
+++ js/consejos-saludables.js
@@ -0,0 +1,8 @@
+
+//Funcionalidad Preguntas frecuentes
+function mostrarLeerMas(){
+    document.getElementById("show").style.display = "block";
+}
+function ocultar(){
+    document.getElementById("show").style.display = "none"
+}
