656213edec228bd5a4530a887f0ac47dbec13b79
Marcos-mi <marcosminof22@gmail.com>
2022-10-05 02:18:30 -0300

    agrego captcha

--- js/formulario.js
+++ js/formulario.js
@@ -111,8 +111,8 @@ formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const novedades = document.getElementById('novedades');
-	const terminos = document.getElementById('terminos');
-	if(campos.nombre && campos.apellido && campos.correo && campos.telefono && campos.consulta && terminos.checked && novedades.checked){
+	const autenticador = document.getElementById('autenticador');
+	if(campos.nombre && campos.apellido && campos.correo && campos.telefono && campos.consulta && autenticador.checked && novedades.checked){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
